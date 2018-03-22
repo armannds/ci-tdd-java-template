632d2b0f30163286b07acc97b1e0183f431434bd
Armann Sigurdsson <armann.sigurdsson@fast2.se>
2018-03-22 18:49:37 +0100

    First and only commit

--- src/test/java/se/cygni/palmithor/tdd/CalculatorTest.java
+++ src/test/java/se/cygni/palmithor/tdd/CalculatorTest.java
@@ -3,6 +3,7 @@ package se.cygni.palmithor.tdd;
 
 import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -13,8 +14,10 @@ public class CalculatorTest {
 
     @Test
     public void test() {
-        assertThat(calculator.sumAll().get()).isEqualTo(1); // TODO failing on purpose please fix
+
+        assertThat(calculator.sumAll(null).isPresent()).isFalse();
+        assertThat(calculator.sumAll(new Integer[0]).get()).isEqualTo(0);
+        assertThat(calculator.sumAll(1,2).get()).isEqualTo(3);
+        assertThat(calculator.sumAll(1,2,3,4).get()).isEqualTo(10);
     }
-
-
 }
